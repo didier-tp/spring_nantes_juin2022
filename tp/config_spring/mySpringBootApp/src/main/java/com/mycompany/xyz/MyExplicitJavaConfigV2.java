0f9a2f38bdfa411c29da8e848bb40654e3951c71
DidierDefrance <didier@d-defrance.fr>
2022-06-22 14:56:04 +0200

    tp1-a

--- tp/config_spring/mySpringBootApp/src/main/java/com/mycompany/xyz/MyExplicitJavaConfigV2.java
+++ tp/config_spring/mySpringBootApp/src/main/java/com/mycompany/xyz/MyExplicitJavaConfigV2.java
@@ -2,31 +2,29 @@ package com.mycompany.xyz;
 
 import org.mygeneric.encadreur.Prefixeur;
 import org.mygeneric.encadreur.Suffixeur;
-import org.mygeneric.encadreur.autoconfigure.MyEncadreurAutoConfiguration;
 import org.mygeneric.encadreur.impl.PrefixeurAvecSeparateurEgal;
 import org.mygeneric.encadreur.impl.SuffixeurAvecSperateurEgal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@Configuration
+//@Configuration
+
 //pas besoin de @Import({MyEncadreurAutoConfiguration.class})
 //car META-INF/spring.factories mentionnant
 //....autoconfigure.EnableAutoConfiguration=...MyEncadreurAutoConfiguration
-//présent dans encadreur-starter.
+//présent dans encadreur-starter.jar
 public class MyExplicitJavaConfigV2 {
 	
 	//NB: si on définit ici les beans Prefixeur et Suffixeur
 	//alors ceux-ci (en tant que config. explicite) l'emportent
-	//sur la configuration automatique de encadreur--with-auto-config.jar
+	//sur la configuration automatique de encadreur-starter.jar
 	
 	/*
 	@Bean
 	public Prefixeur monPrefixeurSpringAvecEgal() {
 		return new PrefixeurAvecSeparateurEgal("**");
 	}
-    */
-	/* 
+	
 	@Bean
 	public Suffixeur monSuffixeurSpringAvecEgal() {
 		return new SuffixeurAvecSperateurEgal("**");
